598816dd3bf894ee39a22047b6cad19e57f6fbcc
Katanajay <jasonmcaulfield@gmail.com>
2025-11-06 23:55:53 +0000

    Add Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://katanajay.github.io/An-Post-Christmas',
+  base: '/An-Post-Christmas',
+  output: 'static'
+});
